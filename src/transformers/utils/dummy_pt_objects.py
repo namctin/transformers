19f3fe10d526de8cfe80bb6fc43f8a78b44a3bd2
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2023-11-29 09:27:46 -0500

    add AutoModelForTimeSeriesPrediction

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -630,6 +630,9 @@ MODEL_FOR_TEXT_TO_WAVEFORM_MAPPING = None
 MODEL_FOR_TIME_SERIES_CLASSIFICATION_MAPPING = None
 
 
+MODEL_FOR_TIME_SERIES_PREDICTION_MAPPING = None
+
+
 MODEL_FOR_TIME_SERIES_REGRESSION_MAPPING = None
 
 
@@ -857,6 +860,13 @@ class AutoModelForTextToSpectrogram(metaclass=DummyObject):
 
 
 class AutoModelForTextToWaveform(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class AutoModelForTimeSeriesPrediction(metaclass=DummyObject):
     _backends = ["torch"]
 
     def __init__(self, *args, **kwargs):
